6dbece41cfc184d04ec753d2a0cc5fe558250451
lsy3709 <lsy3709@naver.com>
2023-12-13 12:26:18 +0900

    4교시 쿼리 조건, all, elemMatch ,slice, reges 등 확인

--- ch3_query/test1.mongodb.js
+++ ch3_query/test1.mongodb.js
@@ -85,12 +85,55 @@ db.stores.find({ $text: { $search: "-java shop coffee" } });
 // tags가 "red", "blank" 둘 다 주어진 순서대로 가진 도큐먼트 전부 출력
 db.inventory.find({ tags: ["red", "blank"] });
 
-db.collection.find({ tags: { $gt: 10, $lt: 5 } });
-db.collection.find({ tags: { $elemMatch: { $gt: 10, $lt: 5 } } });
-db.inventory.insertMany([
+db.collection.insertMany([
   { item: "journal", qty: 25, tags: [10, 20, 30, 40, 50] },
   { item: "notebook", qty: 50, tags: [1, 2, 3, 4, 5] },
   { item: "paper", qty: 100, tags: [3, 6, 9, 12, 15] },
   { item: "planner", qty: 75, tags: [7, 8, 9, 10, 11] },
   { item: "postcard", qty: 45, tags: [2, 4, 6, 8, 10] },
 ]);
+
+// all  x > 10
+// all  x < 15
+db.collection.find({ tags: { $gt: 10, $lt: 15 } });
+// elemMatch : 10 < x < 15 : 11,12,13,14
+db.collection.find({ tags: { $elemMatch: { $gt: 10, $lt: 15 } } });
+
+db.by_month.find({
+  $and: [
+    { month_data: { $elemMatch: { month: "01월", heavy_injury: 0 } } },
+    { month_data: { $elemMatch: { month: "02월", death_toll: 0 } } },
+  ],
+});
+
+// 순서가 중요함
+db.inventory.find({ tags: ["red", "blank"] });
+db.inventory.find({ tags: ["blank", "red"] });
+// $all을 쓰면 "red", "blank" 순서와 상관 없이 해당 요소가 있는지만 확인
+db.inventory.find({ tags: { $all: ["red", "blank"] } });
+
+db.collection.find({ tags: { $size: 5 } }); //배열 lenth가 3인 문서
+
+// {item: "book", tags: ["red", "blank"]}
+// 잘못됨. tags의 첫번째 인자[0]가 아니라 tags 배열의 0이란 원소를 출력하라는 의미
+db.collection.find({}, { "tags.10": 1 });
+
+// tags 배열의 [0], [1]을 출력하라 (앞에서 부터 2개를 출력하라)
+db.collection.find({}, { tags: { $slice: 2 } });
+
+// tags 배열의 [2:3] 을 출력하라
+// $slice: [2, 1]  : 2: 인덱스 번호
+// 1 : 가지고 올 갯수
+db.collection.find({}, { tags: { $slice: [2, 1] } });
+
+// test #m 사용
+db.inventory.find({ item: /^p/i });
+
+// #특정 조건에 부합하는 필드만 출력하라
+// 확인 필요.
+db.inventory.find(
+  {},
+  { tags: { $elemMatch: { $regex: /^b/ } }, _id: 0, item: 0, qty: 0 }
+);
+// #특정 조건에 부합하는 첫번째 데이터만 출력하라
+db.inventory.find({ tags: "red" }, { "tags.$": true });
