1132439e794d306706c1ef7a901f0f318f1fb956
lsy3709 <lsy3709@naver.com>
2023-12-14 11:05:16 +0900

    3교시 aggregate , 집계파이프라인 기본 샘플디비

---
+++ ch6_aggregate/test.mongodb.js
@@ -0,0 +1,12 @@
+db.rating.insertMany([
+  { _id: 1, rating: 1, user_id: 2 },
+  { _id: 2, rating: 2, user_id: 3 },
+  { _id: 3, rating: 3, user_id: 4 },
+  { _id: 4, rating: 3, user_id: 1 },
+  { _id: 5, rating: 4, user_id: 5 },
+  { _id: 6, rating: 4, user_id: 8 },
+  { _id: 7, rating: 5, user_id: 9 },
+  { _id: 8, rating: 5, user_id: 10 },
+  { _id: 9, rating: 5, user_id: 11 },
+  { _id: 10, rating: 5, user_id: 12 },
+]);
